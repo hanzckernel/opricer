675f7820a851c7a5094c6c1d31b241501f60c759
Kernel Han <hanzc.kernel@gmail.com>
2019-06-02 12:17:52 +0200

    Multiple-asset w/o correlation finalised

--- opricer/simulation.py
+++ opricer/simulation.py
@@ -13,21 +13,27 @@ from scipy.linalg import lu_solve, lu_factor
 
 np.random.seed(123)
 a = models.Underlying(datetime.datetime(2010, 1, 1), 100)
+a1 = models.Underlying(datetime.datetime(2010, 1, 1), 100)
 b = models.EurOption(datetime.datetime(2011, 1, 1), 'call')
-c = models.AmeOption(datetime.datetime(2011, 1, 1), 'call')
+b1 = models.AmeOption(datetime.datetime(2011, 1, 1), 'call')
+c = models.BasketOption(datetime.datetime(2011, 1, 1), 'call')
 d = models.BarOption(datetime.datetime(2011, 1, 1), 'put')
 b._attach_asset(100, a)
-c._attach_asset(100, a)
-d._attach_asset([30, np.inf], 100, a)
+b1._attach_asset(100, a)
+c._attach_asset(100, a, a1)
+# d._attach_asset([30, np.inf], 100, a)
 solver = analytics.AnalyticSolver(high_val=2, low_val=0)
 price = solver(b)
 # solver1 = pde.EurSolver()
 solver2 = pde.AmeSolver(high_val=2, low_val=0)
-AMeprice = solver2(c)
+# AMeprice = solver2()
 Msolver = mc.EurMCSolver(path_no=60000, asset_no=10,
                          time_no=100, high_val=2, low_val=0)
 solver4 = pde.BarSolver(high_val=2, low_val=0, asset_no=solver.asset_no)
 Msolver2 = mc.BarMCSolver(high_val=2, low_val=0, asset_no=solver.asset_no)
+Msolver3 = mc.BasketMCSolver(high_val=2, low_val=0, asset_no=solver.asset_no)
+ABSolver = mc.BasketAmeSolver(high_val=2, low_val=0, asset_no=solver.asset_no)
+ASolver = mc.AmeMCSolver(high_val=2, low_val=0, asset_no=solver.asset_no)
 
 
 def plot(options, solvers, Msolvers, with_cursor=False):
@@ -36,11 +42,12 @@ def plot(options, solvers, Msolvers, with_cursor=False):
     price = solver(b)
     MCprice = Msolver(b)
     ax.plot(solver.asset_samples, price, label='AnalyticSol')
-    ax.plot(Msolver.asset_samples, MCprice, label='MC')
-    ax.plot(solver.asset_samples, AMeprice[0], label='AMe')
-    for opt, sol in zip(options, solvers):
-        ax.plot(solver.asset_samples, sol(opt)[0], label=type(
-            sol).__name__ + type(opt).__name__)
+    # ax.plot(solver.asset_samples, ABSolver(c), label='BasketAme')
+    # ax.plot(solver.asset_samples, MCprice, label='MC')
+    # ax.plot(solver.asset_samples, AMeprice[0], label='AMe')
+    # for opt, sol in zip(options, solvers):
+    #     ax.plot(solver.asset_samples, sol(opt)[0], label=type(
+    #         sol).__name__ + type(opt).__name__)
     for opt, sol in zip(options, Msolvers):
         ax.plot(solver.asset_samples, sol(opt), label=type(
             sol).__name__ + type(opt).__name__)
@@ -51,14 +58,8 @@ def plot(options, solvers, Msolvers, with_cursor=False):
     plt.gcf()
 
 
-# solver2(c)
-# print(solver2.__dict__)
-# solver2(c)
-# print(c.__dict__)
-# plot([], [], [])
-# plot([b], [solver2], [Msolver])
-plot([d], [solver4], [Msolver2])
-
+plot([b1, c], [], [ASolver, ABSolver])
+# print(ABSolver(c), ABSolver.__dict__, Msolver3(c), Msolver3.__dict__)
 # price = solver(b)
 # MCprice = Msolver1(b)
 # print(MCprice/price.flatten())

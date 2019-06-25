c2796b1d94d85c42435739c02a07e25cf5656f57
Kernel Han <hanzc.kernel@gmail.com>
2019-06-25 18:26:55 +0200

    first option 3d plot finalised

--- Utils/app.py
+++ Utils/app.py
@@ -9,21 +9,62 @@ import dateutil.parser
 
 # from sfManager import sf_Manager
 
+import dash
+import dash_html_components as html
+
+
+class CustomDash(dash.Dash):
+    def interpolate_index(self, **kwargs):
+        # Inspect the arguments by printing them
+        kwargs['app_entry'] = '''<div id="react-entry-point">
+                                <div class="_dash-loading">
+                                    <div class="circle"></div>
+                                    <div class="circle"></div>
+                                </div>
+                                </div>'''
+        return '''
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+                <title>Option Pricing Tool</title>
+                <link rel="shortcut icon" type="image/png" href="assets/spartan.png"/>
+                <title>Option Pricing</title>
+                {css}
+            </head>
+            <body>
+                {app_entry}
+                {config}
+                {scripts}
+                {renderer}
+            </body>
+        </html>
+        '''.format(
+            css=kwargs['css'],
+            app_entry=kwargs['app_entry'],
+            config=kwargs['config'],
+            scripts=kwargs['scripts'],
+            renderer=kwargs['renderer'])
+
+
 server = flask.Flask(__name__)
-app = dash.Dash(__name__, server=server)
+app = CustomDash(__name__, server=server)
 app.config.suppress_callback_exceptions = True
+
 
 # sf_manager = sf_Manager()
 
-millnames = ["", " K", " M", " B", " T"] # used to convert numbers
+millnames = ["", " K", " M", " B", " T"]  # used to convert numbers
 
 
-# return html Table with dataframe values  
+# return html Table with dataframe values
 def df_to_table(df):
     return html.Table(
         # Header
         [html.Tr([html.Th(col) for col in df.columns])] +
-        
+
         # Body
         [
             html.Tr(
@@ -37,33 +78,34 @@ def df_to_table(df):
     )
 
 
-#returns most significant part of a number
+# returns most significant part of a number
 def millify(n):
     n = float(n)
     millidx = max(
         0,
         min(
-            len(millnames) - 1, int(math.floor(0 if n == 0 else math.log10(abs(n)) / 3))
+            len(millnames) -
+            1, int(math.floor(0 if n == 0 else math.log10(abs(n)) / 3))
         ),
     )
 
     return "{:.0f}{}".format(n / 10 ** (3 * millidx), millnames[millidx])
 
 
-#returns top indicator div
+# returns top indicator div
 def indicator(color, text, id_value):
     return html.Div(
         [
-            
+
             html.P(
                 text,
                 className="twelve columns indicator_text"
             ),
             html.P(
-                id = id_value,
+                id=id_value,
                 className="indicator_value"
             ),
         ],
         className="four columns indicator",
-        
+
     )
--- index.py
+++ index.py
@@ -13,60 +13,73 @@ from Utils.app import app, server
 from Utils.apps import stock, options
 # from apps import cases
 
+
 app.layout = html.Div(
     [
         # header
-        html.Div([
+        html.Div(
+            [
+                html.Div([
 
-            html.Span("Option Pricing Tool",
-                      className='app-title ten columns'),
-            html.Div(
-                html.Img(src=app.get_asset_url('markt.png'), height="80%"),
-                style={"float": "right", "height": "100%", "padding-top": '1%'},
-                className='two columns'),
-            html.P('pricing app powered by Numpy & Dash')
-        ],
-            className="row header"
-        ),
-
-        # tabs
-        html.Div([
-
-            dcc.Tabs(
-                id="tabs",
-                style={"height": "20", "verticalAlign": "middle"},
-                children=[
-                    dcc.Tab(label="Stock Market", value="stock_tab"),
-                    dcc.Tab(label="Option Pricing", value="option_tab"),
-                    # dcc.Tab(id="cases_tab", label="Cases", value="cases_tab"),
+                    html.Span("Option Pricing Tool",
+                              className='app-title ten columns'),
+                    html.Div(
+                        html.Img(src=app.get_asset_url(
+                            'markt.png'), height="80%"),
+                        style={"float": "right", "height": "100%",
+                                        "padding-top": '1%'},
+                        className='two columns'),
+                    html.P('pricing app powered by Numpy & Dash')
                 ],
-                value="option_tab",
-            )
-        ],
-            className="row tabs_div"
+                    className="row header"
+                ),
+
+                # tabs
+                html.Div([
+
+                    dcc.Tabs(
+                        id="tabs",
+                        style={"height": "20",
+                               "verticalAlign": "middle"},
+                        children=[
+                            dcc.Tab(label="Stock Market",
+                                    value="stock_tab"),
+                            dcc.Tab(label="Option Pricing",
+                                    value="option_tab"),
+                            # dcc.Tab(id="cases_tab", label="Cases", value="cases_tab"),
+                        ],
+                        value="option_tab",
+                    )
+                ],
+                    className="row tabs_div"
+                ),
+
+
+                # divs that save dataframe for each tab
+                html.Div(
+                    # sf_manager.get_stock().to_json(orient="split"),  # stock df
+                    id="stock_df",
+                    style={"display": "none"},
+                ),
+                html.Div(
+                    # sf_manager.get_leads().to_json(orient="split"),
+                    id="leads_df", style={"display": "none"}),  # leads df
+                # html.Div(
+                # sf_manager.get_cases().to_json(orient="split"),
+                # id="cases_df", style={"display": "none"}),  # cases df
+
+
+
+                # Tab content
+                html.Div(id="tab_content", className="row",
+                         style={"margin": "2% 3%"}),
+
+                html.Footer(
+                    'Endenicher Allee 60, Bonn • MIT License • hanzc.kernel@gmail.com'),
+            ],
+            className='row',
+            style={"margin": "0%"},
         ),
-
-
-        # divs that save dataframe for each tab
-        html.Div(
-            # sf_manager.get_stock().to_json(orient="split"),  # stock df
-            id="stock_df",
-            style={"display": "none"},
-        ),
-        html.Div(
-            # sf_manager.get_leads().to_json(orient="split"),
-            id="leads_df", style={"display": "none"}),  # leads df
-        # html.Div(
-        # sf_manager.get_cases().to_json(orient="split"),
-        # id="cases_df", style={"display": "none"}),  # cases df
-
-
-
-        # Tab content
-        html.Div(id="tab_content", className="row", style={"margin": "2% 3%"}),
-
-        html.Footer(
-            'Endenicher Allee 60, Bonn • MIT License • hanzc.kernel@gmail.com'),
 
         html.Link(
             href="https://use.fontawesome.com/releases/v5.2.0/css/all.css", rel="stylesheet"),
@@ -78,9 +91,7 @@ app.layout = html.Div(
         html.Link(
             href="https://fonts.googleapis.com/css?family=Ubuntu", rel="stylesheet"),
         # html.Link(href="https://cdn.rawgit.com/amadoukane96/8a8cfdac5d2cecad866952c52a70a50e/raw/cd5a9bf0b30856f4fc7e3812162c74bfc0ebe011/dash_crm.css", rel="stylesheet")
-    ],
-    className="row",
-    style={"margin": "0%"},
+    ], className='main',
 )
 
 
